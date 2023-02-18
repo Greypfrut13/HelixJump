947a6b3bc494194a203e7daa2b3fd02cc057bdce
Riazanov Igor <rebzya33@mail.ru>
2023-02-18 16:57:51 +0800

    Optimized Ball Effects emission

--- Assets/Scripts/Ball/BallEffects.cs
+++ Assets/Scripts/Ball/BallEffects.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 public class BallEffects : MonoBehaviour
 {
     [SerializeField] private ParticleSystem _collisionParticlesPrefab;
+    
+    private ParticleSystem _collisionParticles;
+
+    private void Start() 
+    {
+        _collisionParticles = Instantiate(_collisionParticlesPrefab);
+    }
 
     private void OnCollisionEnter(Collision other)
     {
@@ -13,6 +20,8 @@ public class BallEffects : MonoBehaviour
     private void EmitCollisionParticles(Collision other)
     {
         Vector3 collisionPosition = other.contacts[0].point;
-        Instantiate(_collisionParticlesPrefab, collisionPosition, Quaternion.identity);
+        
+        _collisionParticles.transform.position = collisionPosition;
+        _collisionParticles.Play();
     }
 }
