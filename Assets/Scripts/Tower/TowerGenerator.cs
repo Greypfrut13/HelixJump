ae47331ac9a21d4354650abb10f1160870114c32
Igor Ryazanov <123439439+Greypfrut13@users.noreply.github.com>
2025-07-12 11:11:22 +0800

    Created Start, Finish Platform prefabs

---
+++ Assets/Scripts/Tower/TowerGenerator.cs
@@ -0,0 +1,12 @@
+﻿using Platforms;
+using UnityEngine;
+
+namespace Tower
+{
+    public class TowerGenerator : MonoBehaviour
+    {
+        [SerializeField] private Platform _startPlatform; 
+        [SerializeField] private Platform _finishPlatform; 
+        [SerializeField] private Platform[] _platformsVariants; 
+    }
+}
