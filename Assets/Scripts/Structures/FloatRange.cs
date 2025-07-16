27a19b95794e0c0fcc584e8aeaf150280e77d3a1
Igor Ryazanov <123439439+Greypfrut13@users.noreply.github.com>
2025-07-16 15:19:18 +0800

    Created Tower Generator

---
+++ Assets/Scripts/Structures/FloatRange.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Structures
+{
+    [CreateAssetMenu(fileName = "FloatRange", menuName = "ScriptableObjects/Structures/FloatRange")]
+    public class FloatRange : Range<float>
+    {
+        public float Random => UnityEngine.Random.Range(Min, Max);
+    }
+}
