b44c125a1c4bc01ebb0c44e064fde73049133840
Igor Ryazanov <123439439+Greypfrut13@users.noreply.github.com>
2025-08-06 17:06:23 +0800

    Created Tower Generator

---
+++ Assets/Scripts/Tower/Generation/TowerGenerationData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Platforms;
+using UnityEngine;
+
+namespace Tower
+{
+    [Serializable]
+    public class TowerGenerationData
+    {
+        [SerializeField] private GameObject _towerBase;
+        
+        [Header("Platforms")]
+        [SerializeField] private Platform _startPlatform;
+        [SerializeField] private Platform _finishPlatform;
+        [SerializeField] private Platform[] _platformPrefabs;
+
+        public GameObject TowerBase => _towerBase;
+
+        public Platform StartPlatform => _startPlatform;
+
+        public Platform FinishPlatform => _finishPlatform;
+
+        public Platform[] PlatformPrefabs => _platformPrefabs;
+
+        public Platform GetRandomPlatform()
+        {
+            int randomIndex = UnityEngine.Random.Range(0, _platformPrefabs.Length);
+            
+            return _platformPrefabs[randomIndex];
+        }
+    }
+}
---
+++ Assets/Scripts/Tower/Generation/TowerGenerator.cs
@@ -0,0 +1,40 @@
+﻿using Platforms;
+using UnityEngine;
+
+namespace Tower
+{
+    public class TowerGenerator : MonoBehaviour
+    {
+        [SerializeField] [Min(0)] private int _platformsCount;
+        
+        [SerializeField] private TowerGenerationData _data;
+
+        private void Start()
+        {
+            Generate(_data.TowerBase);
+        }
+
+        private void Generate(GameObject towerBasePrefab)
+        {
+            GameObject towerBase = Instantiate(towerBasePrefab, transform);
+            towerBase.transform.localScale = new Vector3(1f, _platformsCount / 2f, 1f);
+            
+            Vector3 spawnPosition = towerBase.transform.position;
+            spawnPosition.y += towerBase.transform.localScale.y;
+
+            for (int i = 0; i < _platformsCount; i++)
+            {
+                SpawnPlatform(_data.GetRandomPlatform(), ref spawnPosition, transform);
+            }
+        }
+
+        private void SpawnPlatform(Platform platform, ref Vector3 spawnPosition, Transform parent)
+        {
+            Quaternion randomYRotation = Quaternion.Euler(0, Random.Range(0, 360), 0);
+            
+            Platform createdPlatform = Instantiate(platform, spawnPosition, randomYRotation, parent);
+
+            spawnPosition.y -= 1;
+        }
+    }
+}
