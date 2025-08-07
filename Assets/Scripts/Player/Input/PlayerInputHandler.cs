6b8c3a383c2a025adb50a2d5a4e71dd923bfb1dc
Igor Ryazanov <123439439+Greypfrut13@users.noreply.github.com>
2025-08-07 18:39:15 +0800

    Created Tower Rotation

---
+++ Assets/Scripts/Player/Input/PlayerInputHandler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Tower.Rotation;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Player.Input
+{
+    public class PlayerInputHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+    {
+        [SerializeField] private TowerRotator _towerRotator;
+
+        [SerializeField] [Min(0.0f)] private float _maxSensitivity;
+        
+        private Vector3 _startPosition;
+        private float _rotationDirection;
+        
+        private void Update()
+        {
+            _towerRotator.Rotate(_rotationDirection);
+        }
+
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            _startPosition = eventData.position;
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            _rotationDirection = CalculateRotationDirection(eventData);
+        }
+        
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            _rotationDirection = 0;
+        }
+
+        private float CalculateRotationDirection(PointerEventData eventData)
+        {
+            Vector3 currentPosition = eventData.position;
+            float deltaX = currentPosition.x - _startPosition.x;
+
+            float direction = MathF.Sign(deltaX);
+            float rotationStrength = MathF.Min(MathF.Abs(deltaX) / _maxSensitivity, 1f);
+            
+            return rotationStrength * direction;
+        }
+
+        
+    }
+}
---
+++ Assets/Scripts/Tower/Rotation/TowerRotator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace Tower.Rotation
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class TowerRotator : MonoBehaviour
+    {
+        [SerializeField] [Min(0.0f)] private float _rotateSpeed;
+
+        private Rigidbody _rigidbody;
+        
+        private void Start()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+        
+        public void Rotate(float direction)
+        {
+            float torque = -direction * _rotateSpeed;
+            _rigidbody.AddTorque(Vector3.up * torque);
+        }
+    }
+}
