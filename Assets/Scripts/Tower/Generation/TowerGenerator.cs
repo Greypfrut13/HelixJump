140d950794c42e2b8ebad52aa3be7fb547d97055
Igor Ryazanov <123439439+Greypfrut13@users.noreply.github.com>
2025-08-07 14:57:07 +0800

    Added spawn for Start and Finish platforms

--- Assets/Scripts/Tower/Generation/TowerGenerator.cs
+++ Assets/Scripts/Tower/Generation/TowerGenerator.cs
@@ -6,8 +6,11 @@ namespace Tower
     public class TowerGenerator : MonoBehaviour
     {
         [SerializeField] [Min(0)] private int _platformsCount;
+        [SerializeField] [Min(0.0f)] private float _spawnStep;
         
         [SerializeField] private TowerGenerationData _data;
+
+        private const float START_PLATFORM_OFFSET = 0.5f;
 
         private void Start()
         {
@@ -17,15 +20,19 @@ namespace Tower
         private void Generate(GameObject towerBasePrefab)
         {
             GameObject towerBase = Instantiate(towerBasePrefab, transform);
-            towerBase.transform.localScale = new Vector3(1f, _platformsCount / 2f, 1f);
+            towerBase.transform.localScale = new Vector3(1f, _platformsCount / 2f + _spawnStep, 1f);
             
             Vector3 spawnPosition = towerBase.transform.position;
-            spawnPosition.y += towerBase.transform.localScale.y;
+            spawnPosition.y += towerBase.transform.localScale.y - START_PLATFORM_OFFSET;
+
+            SpawnPlatform(_data.StartPlatform, ref spawnPosition, transform);
 
             for (int i = 0; i < _platformsCount; i++)
             {
                 SpawnPlatform(_data.GetRandomPlatform(), ref spawnPosition, transform);
             }
+            
+            SpawnPlatform(_data.FinishPlatform, ref spawnPosition, transform);
         }
 
         private void SpawnPlatform(Platform platform, ref Vector3 spawnPosition, Transform parent)
@@ -34,7 +41,7 @@ namespace Tower
             
             Platform createdPlatform = Instantiate(platform, spawnPosition, randomYRotation, parent);
 
-            spawnPosition.y -= 1;
+            spawnPosition.y -= _spawnStep;
         }
     }
 }
